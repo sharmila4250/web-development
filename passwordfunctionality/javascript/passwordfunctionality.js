c61dac8d205e81c69089039714a0f054aa3758b9
Sharmila Samantaray <66352207+sharmila4250@users.noreply.github.com>
2021-03-28 09:17:02 +0530

    Create passwordfunctionality.js

---
+++ passwordfunctionality/javascript/passwordfunctionality.js
@@ -0,0 +1,8 @@
+function showPassword(){
+var ele=document.getElementById("pass");
+ele.type="text";
+}
+function hidePassword(){
+    var ele=document.getElementById("pass");
+    ele.type="password";
+    }
